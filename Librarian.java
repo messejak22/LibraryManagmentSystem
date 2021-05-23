ff29954d9091c277cc8230ef5fa9f9a233a0e913
messejak22 <70961425+messejak22@users.noreply.github.com>
2021-05-23 17:19:29 -0400

    Update Librarian.java

--- Librarian.java
+++ Librarian.java
@@ -5,6 +5,20 @@ public class Librarian extends Person {
     }
 
 
+    public static void isbnChecker(String isbn) {
+        // if x 
+        int sum = 0;
+        if (isbn.contains("X") == true) {
 
+        } else {
+            // no x   
+            int isbn2 = Integer.valueOf(isbn);
+            int k = 0;
+            for (int i = 10; i > isbn.length(); i--) {
+                 sum = sum + i*(Integer.valueOf(isbn.charAt(k)));
+                 k++;
+            }
+        }
+    }
     
 }
